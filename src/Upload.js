83b7a37f408705dca9ad7073b99c26d943720068
chchunleon <chchunleon@gmail.com>
2021-07-21 09:44:17 +0800

    include validation

--- src/Upload.js
+++ src/Upload.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "antd/dist/antd.css";
 import './index.css';
-import { Space, Divider, Radio, Row, Col, Input, Layout, Upload, Button } from 'antd';
+import { Space, Divider, Radio, Row, Col, Input, InputNumber, Layout, Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -13,8 +13,12 @@ const { Header, Footer, Sider, Content } = Layout;
 class UploadVideo extends Component {
     constructor () {
         super();
-        this.file = null;
-        this.shootingFoot = "right";
+        this.state = {
+            file: null,
+            height: 170,
+            shootingFoot: "right",
+            isAnalysing: false
+        }
     }
 
     changeVideo = (e) => {
@@ -22,13 +26,24 @@ class UploadVideo extends Component {
     }
 
     beforeUpload = (file, fileList) => {
-        this.file = file
-        console.log('beforeupload', file, fileList, this.file)
+        this.setState({ file: file })
+        console.log('beforeupload', file, fileList, this.state.file)
         return false
     }
 
+    changeHeight = e => {
+        this.setState({ height: e })
+        console.log(this.state.height, this.state.shootingFoot, this.state.file, e)
+        if (this.state.height == null)
+            console.log('height null')
+        if (this.state.shootingFoot == null)
+            console.log('shootingFoot null')
+        if (!this.state.file && this.state.file == null)
+            console.log('file null')
+    }
+
     changeFoot = e => {
-        this.shootingFoot = e.target.value
+        this.setState({ shootingFoot: e.target.value })
         // setValue(e.target.value);
     }
 
@@ -50,6 +65,7 @@ class UploadVideo extends Component {
                     // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                     change={this.changeVideo}
                     multiple={false}
+                    maxCount={1}
                 >
                     <Button icon={<UploadOutlined />}>Upload your video</Button>
                 </Upload>
@@ -57,14 +73,14 @@ class UploadVideo extends Component {
                 <Row>
                     <Col span={12}>Your height:</Col>
                     <Col span={12}>
-                        <Input placeholder="cm" />
+                        <InputNumber min={130} max={200} defaultValue={170} onChange={this.changeHeight} /> cm
                     </Col>
                 </Row>
                 <br />
                 <Row type="flex" align="middle">
                     <Col span={12}>Your shooting foot:</Col>
                     <Col span={12}>
-                        <Radio.Group onChange={this.changeFoot}>
+                        <Radio.Group onChange={this.changeFoot} defaultValue={this.state.shootingFoot}>
                             <Radio value={"left"}>Left</Radio>
                             <Radio value={"right"}>Right</Radio>
                         </Radio.Group>
@@ -73,7 +89,7 @@ class UploadVideo extends Component {
                 <br />
                 <Row type="flex" align="middle">
                     <Col>
-                        <Button type="primary" shape="round" onClick={this.clickAnalysis}>
+                        <Button type="primary" shape="round" onClick={this.clickAnalysis} disabled={this.state.file == null || this.state.height == null}>
                             Analyse
                         </Button>
                     </Col>
